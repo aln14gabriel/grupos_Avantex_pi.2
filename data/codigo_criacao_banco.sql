caacf9382af3e050542cbe29f03a7a93c639318c
Ruanvins <138736761+Ruanvins@users.noreply.github.com>
2025-04-05 00:40:29 -0300

    Atualização Banco de dados
    Tentativa 0.1 banco de dados

--- data/codigo_criacao_banco.sql
+++ data/codigo_criacao_banco.sql
@@ -1,1 +1,6 @@
 -- Coloque neste arquivo todo o código de criação do banco de dados
+
+| Coluna | Tipo | Descrição |
+|--------|------|-----------|
+| col_1 | varchar | Coluna de texto |
+| col_2 | int | Coluna de números inteiros |
