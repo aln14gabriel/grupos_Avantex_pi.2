91a183e969c0b133819a953a2cb3034dd12568c4
Ruanvins <138736761+Ruanvins@users.noreply.github.com>
2025-06-04 12:12:07 -0300

    Update Funcoes.java
    conexão com o banco e com as outras classes DAO

--- src/control/Funcoes.java
+++ src/control/Funcoes.java
@@ -1,13 +1,12 @@
-package src.control;
+package src.dao;
 
-   import java.sql.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Funcoes {
-
 public class ConnectionFactory {
 
+    
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/academia?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASS = "1234"; 
