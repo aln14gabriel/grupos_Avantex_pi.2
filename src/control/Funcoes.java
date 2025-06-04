281be8f7ea6a36353b3c91a65c0fb9b68bd9a2c3
Ruanvins <138736761+Ruanvins@users.noreply.github.com>
2025-06-04 02:14:25 -0300

    Update Funcoes.java
    Adicionado a função de conexão ao banco

--- src/control/Funcoes.java
+++ src/control/Funcoes.java
@@ -1,6 +1,32 @@
 package src.control;
 
+   import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Funcoes {
-    // Crie aqui as funções que irão realizar a comunicação com o banco de dados
-    // Faça como métodos estáticos
+
+public class ConnectionFactory {
+
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/academia?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "1234"; 
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("ERRO: Driver MySQL não encontrado!");
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            System.err.println("ERRO NA CONEXÃO:");
+            throw new RuntimeException("Falha na conexão", e);
+        }
+    }
 }
